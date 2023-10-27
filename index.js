e0ec29eeda68d86b50f861fd930ad305ec88d194
Jubair <jubair.ahmed2838@gmail.com>
2023-10-27 18:21:10 +0600

    Add: token created with jwt

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-const app = express()
+const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000
-require('dotenv').config()
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser')
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.hf0b3tt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,66 +34,84 @@ async function run() {
     await client.connect();
 
     // service collection
-    const servicesCollection = client.db("carDoctorDB").collection("services")
+    const servicesCollection = client.db("carDoctorDB").collection("services");
     // order collection
-    const ordersCollection = client.db("carDoctorDB").collection("orders")
+    const ordersCollection = client.db("carDoctorDB").collection("orders");
+
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+
+      res
+      .cookie('token', token, {
+        httpOnly:true,
+        secure:false,
+    
+      })
+      .send({success:true})
+    
+    });
 
     // get services endpoint
-    app.get("/services", async(req, res)=>{
-        const cursor = servicesCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // get a single service
-    app.get("/services/:id", async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await servicesCollection.findOne(query)
-        res.send(result)
-    })
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
 
     // get endpoint of orders
-    app.get("/orders", async(req, res)=>{
-        let query = {};
-        if(req.query?.email){
-            query = {email: req.query.email}
-        }
-        const result = await ordersCollection.find(query).toArray()
-        res.send(result)
-    })
+    app.get("/orders", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await ordersCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // post a order by user, POST endpoint
-    app.post("/orders", async(req, res)=>{
-        const order = req.body;
-        const result = await ordersCollection.insertOne(order)
-        res.send(result)
-    })
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
     // order, delete endpoint
-    app.delete("/orders/:id", async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await ordersCollection.deleteOne(query)
-        res.send(result)
-    })
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // order, patch endpoint
-    app.patch("/orders/:id", async(req, res)=>{
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const updateOrder = req.body
-        const updatedDoc = {
-            $set: {
-                status: updateOrder.status
-            }
-        }
-        const result = await ordersCollection.updateOne(filter, updatedDoc)
-        res.send(result)
-    })
-
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateOrder = req.body;
+      const updatedDoc = {
+        $set: {
+          status: updateOrder.status,
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -97,14 +119,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Car doctor server is RUNNING!");
+});
 
-app.get("/", (req, res)=>{
-    res.send("Car doctor server is RUNNING!")
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on ${port}`)
-})
-
-
-
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
