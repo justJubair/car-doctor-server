0b1f5445d6b04c145f7ecb9b58fcaa45eea4036d
Jubair <jubair.ahmed2838@gmail.com>
2023-11-02 01:55:30 +0600

    Fix:console log removed

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function run() {
     // clear cookie api, JWT
     app.post("/logout", (req,res)=>{
       const user = req.body;
-      console.log(user)
+     
       res.clearCookie("token", {maxAge:0}).send({success:true})
     })
 
